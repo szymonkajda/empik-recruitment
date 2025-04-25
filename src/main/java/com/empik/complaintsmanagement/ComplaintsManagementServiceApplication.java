6c6cdca14245279b30846e5d0dd5ad2fea1347b8
skajd <skajda01@gmail.com>
2025-04-25 14:23:18 +0200

    Initial commit.

---
+++ src/main/java/com/empik/complaintsmanagement/ComplaintsManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.empik.complaintsmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplaintsManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComplaintsManagementServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/empik/complaintsmanagement/ComplaintsManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.empik.complaintsmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComplaintsManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
