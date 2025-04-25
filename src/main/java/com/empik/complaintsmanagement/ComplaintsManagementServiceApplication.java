ccedf60000e834dadd9de4aed57f80b0689f8180
skajd <skajda01@gmail.com>
2025-04-25 15:07:25 +0200

    Introduce spotless.

--- src/main/java/com/empik/complaintsmanagement/ComplaintsManagementServiceApplication.java
+++ src/main/java/com/empik/complaintsmanagement/ComplaintsManagementServiceApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ComplaintsManagementServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ComplaintsManagementServiceApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(ComplaintsManagementServiceApplication.class, args);
+  }
 }
